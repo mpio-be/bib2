3e5f0be2fe45d8cc9bfaddf04952bbe92a6702d5
Mihai Valcu <valcu.mihai@gmail.com>
2019-05-13 11:48:07 +0200

    update following DataEntry.validation() framework

--- inst/UI/DataEntry/BTatWESTERHOLZ/ADULTS/global.R
+++ inst/UI/DataEntry/BTatWESTERHOLZ/ADULTS/global.R
@@ -5,7 +5,7 @@
 # ==========================================================================
 
 # settings
-	sapply(c('bib2','DataEntry', 'data.table', 'shinyjs', 'tableHTML', 'glue'),
+	sapply(c('bib2','DataEntry', 'DataEntry.validation', 'data.table', 'shinyjs', 'tableHTML', 'glue'),
 		require, character.only = TRUE, quietly = TRUE)
 	tags = shiny::tags
 
@@ -15,6 +15,7 @@
 	pwd             = sdb::getCredentials(user, db, host )$pwd
 
 	tableName       =  'ADULTS'
+	package         = 'bib2'
 	excludeColumns  = 'ad_pk'
 	n_empty_lines   =  30
 
@@ -38,37 +39,5 @@ comments = column_comment(user, host, db, pwd, tableName,excludeColumns)
 		hot_cols(columnSorting = FALSE, manualColumnResize = TRUE) %>%
 		hot_rows(fixedRowsTop = 1) 
 
-
-	# validator parameters
-	measures      = dbq(user = user, host = host, q = 'select tarsus, weight, P3 from BTatWESTERHOLZ.ADULTS')
-	measures      = melt(measures)[!is.na(value)]
-	measures      = measures[, .(lq = quantile(value, 0.005), uq = quantile(value, 0.995)), by = variable]
-	nchar         = data.table(variable = c('ID', 'UL', 'LL', 'UR', 'LR', 'transponder', 'age', 'sex'), n = c(7, 1, 1, 1, 1, 16, 1, 1) )
-	
-	transponders  = dbq(user = user, host = host, q = "select * from COMMON.TRANSPONDERS_LIST")
-	preDefined1   = data.table( variable = 'transponder', set = c(list(transponders$transponder)))
-	preDefined2   = data.table( variable = 'box',         set = c(list(1:277)))
-
-
-# inspector
-	inspector.ADULTS <- function(forv) {
-		z = copy(forv)
-
-		i1 = is.na_validator(z[, .(date_time_caught, author)])
-		i2 = is.na_validator(z[is.na(recapture), .(age,tarsus,weight,P3,transponder)])[, reason := 'mandatory at first recapture']
-		i3 = POSIXct_validator(z[ , .(date_time_caught)])
-		i4 = hhmm_validator(z[ , .(handling_start,handling_stop,release_time)])
-		i5 = interval_validator(z[ , .(tarsus,P3, weight)], measures)
-		i6 = nchar_validator(z[ , .(ID,UL,LL,UR,LR,transponder,age,sex)], nchar)
-		
-		i7 = is.element_validator(z, preDefined1, reason = 'transponder does not exist!')
-		i8 = is.element_validator(z, preDefined2, reason = 'box does not exist!')
-
-
-		o = list(i1, i2, i3, i4, i5, i6, i7, i8) %>% rbindlist
-		o = o[, .(rowid = paste(rowid, collapse = ',')), by = .(variable, reason)]
-		o
-
-		}
 
 
--- inst/UI/DataEntry/BTatWESTERHOLZ/NESTS/global.R
+++ inst/UI/DataEntry/BTatWESTERHOLZ/NESTS/global.R
@@ -6,7 +6,7 @@
 
 
 # settings
-  sapply(c('bib2','DataEntry', 'data.table', 'shinyjs', 'shinyBS'),
+  sapply(c('bib2','DataEntry', 'DataEntry.validation', 'data.table', 'shinyjs', 'shinyBS'),
     require, character.only = TRUE, quietly = TRUE)
 
   host            =  getOption('host.bib2') 
@@ -15,6 +15,7 @@
   pwd             = sdb::getCredentials(user, db, host )$pwd
   
   tableName       =  'NESTS'
+  package         = 'bib2'
   excludeColumns = 'N_pk'
   n_empty_lines   = 60
 
@@ -44,35 +45,5 @@ comments = column_comment(user, host, db, pwd, tableName,excludeColumns)
     hot_col(col = "nest_stage", type = "dropdown", source = nest_stages ) %>%
     hot_col(col = "author", type = "dropdown", source = authors )
 
-
-# validator parameters
-    nest_failed_reasons = c('R', 'P', 'D', 'H', 'U')
-
-# inspector 
-  inspector.NESTS <- function(x) {
-
-    v1 = is.na_validator(x[, .(date_time, author, box, nest_stage)])
-    v2 = POSIXct_validator(x[ , .(date_time)] )
-    
-    v3 = is.element_validator(x[ , .(nest_stage)], data.table(variable = 'nest_stage', set = list(nest_stages) ))
-    v4 = is.element_validator(x[ , .(nest_failed)], data.table(variable = 'nest_failed', set =  list(nest_failed_reasons) ))
-    v5 = is.element_validator(x[ , .(authors)], data.table(variable = 'authors', set = list( authors ) ))
-
-    v6 = interval_validator( x[, .(box)], v = data.table(variable = 'box', lq = 1, uq = 277 ) )
-    colNams = c('femaleLeft', 'warm_eggs','eggs_covered') 
-    v7 = interval_validator(subset(x, select = colNams) , data.table(variable = colNams , lq = 0, uq = 1 ) )
-    
-    colNams = c('eggs', 'chicks', 'age_chicks_processing',  'collect_eggs', 'dead_eggs', 'dead_chicks')
-    vvv = data.table(variable = colNams , lq = c(1,1, 13, 1, 1, 1), uq = c(14,14, 15, 15, 15, 15) ) 
-    v8 = interval_validator(subset(x, select = colNams) ,  vvv)
-
-    v9 = interval_validator(subset(x, select = 'female_inside_box') , data.table(variable = 'female_inside_box' , lq = 1, uq = 2 ) )
-    
-    colNams = c('herbs', 'guessed')
-    v10 = is.identical_validator(subset(x, select = colNams) , data.table(variable = colNams , x = 1) )
- 
-    o = list(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) %>% rbindlist
-    o[, .(rowid = paste(rowid, collapse = ',')), by = .(variable, reason)]
-    }
 
 
