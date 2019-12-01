df67f0027546ec233b5225d794736ed6c7e999e4
Mihai <valcu.mihai@gmail.com>
2019-12-01 15:28:32 +0100

    ui update

--- inst/UI/main/global.R
+++ inst/UI/main/global.R
@@ -2,10 +2,13 @@
 # shiny::runApp('inst/UI/main', launch.browser = TRUE)
 
 # settings
-    sapply(c('bib2','SNB2', 'knitr', 'ggplot2', 'ggthemes','digest','ggrepel', 'MASS',
+    sapply(c('bib2','SNB2',   'MASS',
+             'magrittr', 'stringr', 'knitr', 'glue'  , 
+             'ggplot2', 'ggthemes','digest','ggrepel',
              'shiny','shinyjs','shinydashboard','shinyBS','shinytoastr', 'shinyTree', 'shinyAce'),
       function(x) suppressPackageStartupMessages( require(x, character.only = TRUE, quietly = TRUE) ))
     options(stringsAsFactors = FALSE)
+
 
 
 # global sets
