1280cc6c56d7101e28d58d1d9083b34b8e1f9e08
valcu <valcu@orn.mpg.de>
2020-03-26 20:23:08 +0100

    NESTS DataEntry UI updated

--- inst/UI/DataEntry/BTatWESTERHOLZ/NESTS/global.R
+++ inst/UI/DataEntry/BTatWESTERHOLZ/NESTS/global.R
@@ -21,16 +21,15 @@ comments = column_comment(user, host, db, pwd, tableName,excludeColumns)
 
 # Define UI table  
 nest_stages = c('NOTA','WSP', 'U', 'LT','R','B','BC','C','LIN','E','Y')
-authors = dbq(user = user, host = host, q = paste0('SELECT initials from ', db, '.AUTHORS UNION 
-                    SELECT distinct initials from BTatWESTERHOLZ.AUTHORS') )$initials
+# authors = dbq(user = user, host = host, q = paste0('SELECT initials from ', db, '.AUTHORS UNION  SELECT distinct initials from BTatWESTERHOLZ.AUTHORS') )$initials
 
 uitable = emptyFrame(user, host, db, pwd, tableName, n = n_empty_lines, excludeColumns, 
 preFilled = list( date_time = as.character(Sys.Date()) ) ) %>% 
 rhandsontable %>% 
 hot_cols(columnSorting = FALSE, manualColumnResize = TRUE) %>%
-hot_rows(fixedRowsTop = 1) %>%
-hot_col(col = "nest_stage", type = "dropdown", source = nest_stages ) %>%
-hot_col(col = "author", type = "dropdown", source = authors )
+hot_rows(fixedRowsTop = 1) # %>%
+# hot_col(col = "nest_stage", type = "dropdown", source = nest_stages ) %>%
+# hot_col(col = "author", type = "dropdown", source = authors )
 
 
 
